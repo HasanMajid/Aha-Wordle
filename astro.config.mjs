db975947643fe4884c0310cbebec8a7954bf5ef9
Hasan Majid <hasanmajid3.14@hotmail.com>
2024-04-27 07:03:42 -0400

    added alpine js

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+import alpinejs from "@astrojs/alpinejs";
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [alpinejs()],
+  output: "server"
+});
